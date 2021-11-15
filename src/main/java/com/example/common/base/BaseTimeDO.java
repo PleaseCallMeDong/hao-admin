62a09fd0d3c3328c481784541f4b5073e05ff0b3
dj <buyaotaichang@vip.qq.com>
2021-11-15 16:53:37 +0800

    feat: MyBatis-Plus 自动填充功能

--- src/main/java/com/example/common/base/BaseTimeDO.java
+++ src/main/java/com/example/common/base/BaseTimeDO.java
@@ -1,6 +1,9 @@
 package com.example.common.base;
 
 import com.alibaba.fastjson.annotation.JSONField;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -18,12 +21,16 @@ public class BaseTimeDO implements Serializable {
      * 创建时间
      */
     @JSONField(serialize = false)
+    @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     /**
      * 修改时间
      */
     @JSONField(serialize = false)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
 }
---
+++ src/main/java/com/example/common/handler/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.example.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author dj
+ * @date 2021-11-15 16:50
+ * @description MyBatis-Plus 自动填充功能
+ **/
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
